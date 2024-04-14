69b752274e3fd67d3b938a5e8e0bb5d23a2d24b3
RJUNCC <20bigal04@ryeguytech.com>
2024-04-14 12:19:41 -0400

    added zoom auto join files

---
+++ personal_projects/auto_join_zoom/app/data.py
@@ -0,0 +1,3 @@
+lst = [
+    ["https://olympus1.mygreatlearning.com/online_session/join_meeting?access_token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJwcm9ncmFtX2dyb3VwX2lkIjoxNTg2NiwibG1zX3VzZXJfaWQiOjEwNDU4MTA2LCJtZW50b3JlZF9zZXNzaW9uX2lkIjoxMjAzMzQ0LCJleHAiOjE3MTMxMDk1MDB9.t_Wco-vXXQ64Rbyl5YhjbB-5Tlc6VkL8fUhHzDxHnKc&utm_source=google_calendar&utm_medium=student_join_session", "9:00", "12:00"]
+]
